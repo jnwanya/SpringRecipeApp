eaaa4cd1a32327ab89978ba85b72d54e08b4cf9b
Nwanya Justin <nwanyajustin@gmail.com>
2018-01-11 14:46:39 +0100

    Implemented image save to database

--- src/main/java/com/jnwanya/recipe/services/ImageServiceImpl.java
+++ src/main/java/com/jnwanya/recipe/services/ImageServiceImpl.java
@@ -1,8 +1,13 @@
 package com.jnwanya.recipe.services;
 
+import com.jnwanya.recipe.domain.Recipe;
+import com.jnwanya.recipe.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by Jnwanya on
@@ -12,8 +17,34 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
        log.debug("Recieved a file");
+
+       try{
+           Recipe recipe = recipeRepository.findById(recipeId).get();
+           Byte[] byteObject = new Byte[file.getBytes().length];
+
+           int i = 0;
+
+           for(byte b: file.getBytes()){
+               byteObject[i++] = b;
+           }
+
+           recipe.setImage(byteObject);
+
+           recipeRepository.save(recipe);
+
+       }catch (IOException ex){
+           log.error("Error occured", ex);
+           ex.printStackTrace();
+       }
     }
 }
